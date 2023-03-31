db1532eec730c0bb76205f5bc8eb6bde956f3830
administrador <gilberto.tec@vivaldi.net>
2023-03-31 17:51:24 -0300

    freature: adicionando novos exports para o bacjend

--- dist/index.js
+++ dist/index.js
@@ -36,12 +36,40 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
     }
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.ManagerDatabase = exports.DataSource = void 0;
+exports.ManagerDatabase = exports.Sale = exports.Offer = exports.Position = exports.Building = exports.City = exports.Market = exports.Npc = exports.Player = exports.Province = exports.Resource = exports.World = exports.TypeLogin = exports.Login = exports.User = exports.DataSource = void 0;
 var dotenv = require('dotenv');
 dotenv.config({ path: './.env' });
 var database_1 = require("./database");
 var typeorm_1 = require("typeorm");
 Object.defineProperty(exports, "DataSource", { enumerable: true, get: function () { return typeorm_1.DataSource; } });
+var user_1 = require("./entities/user");
+Object.defineProperty(exports, "User", { enumerable: true, get: function () { return user_1.User; } });
+var login_1 = require("./entities/auth/login");
+Object.defineProperty(exports, "Login", { enumerable: true, get: function () { return login_1.Login; } });
+var type_login_1 = require("./entities/auth/type_login");
+Object.defineProperty(exports, "TypeLogin", { enumerable: true, get: function () { return type_login_1.TypeLogin; } });
+var world_1 = require("./entities/game/world");
+Object.defineProperty(exports, "World", { enumerable: true, get: function () { return world_1.World; } });
+var resource_1 = require("./entities/game/resource");
+Object.defineProperty(exports, "Resource", { enumerable: true, get: function () { return resource_1.Resource; } });
+var province_1 = require("./entities/game/province");
+Object.defineProperty(exports, "Province", { enumerable: true, get: function () { return province_1.Province; } });
+var player_1 = require("./entities/game/player");
+Object.defineProperty(exports, "Player", { enumerable: true, get: function () { return player_1.Player; } });
+var npc_1 = require("./entities/game/npc");
+Object.defineProperty(exports, "Npc", { enumerable: true, get: function () { return npc_1.Npc; } });
+var market_1 = require("./entities/game/market");
+Object.defineProperty(exports, "Market", { enumerable: true, get: function () { return market_1.Market; } });
+var city_1 = require("./entities/game/city");
+Object.defineProperty(exports, "City", { enumerable: true, get: function () { return city_1.City; } });
+var building_1 = require("./entities/game/building");
+Object.defineProperty(exports, "Building", { enumerable: true, get: function () { return building_1.Building; } });
+var position_1 = require("./entities/game/geometry/position");
+Object.defineProperty(exports, "Position", { enumerable: true, get: function () { return position_1.Position; } });
+var offer_1 = require("./entities/game/market/offer");
+Object.defineProperty(exports, "Offer", { enumerable: true, get: function () { return offer_1.Offer; } });
+var sale_1 = require("./entities/game/market/sale");
+Object.defineProperty(exports, "Sale", { enumerable: true, get: function () { return sale_1.Sale; } });
 // Open connection postgress function
 var openConnection = (function (param) {
     if (param === void 0) { param = undefined; }
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,21 @@ import { DataSource } from "typeorm";
 import { buildDatabase, ConectionParameter}  from "./database";
 export { DataSource } from "typeorm";
 
+export { User } from "./entities/user";
+export { Login } from "./entities/auth/login";
+export { TypeLogin } from "./entities/auth/type_login";
+export { World } from "./entities/game/world";
+export { Resource } from "./entities/game/resource";
+export { Province } from "./entities/game/province";
+export { Player } from "./entities/game/player";
+export { Npc } from "./entities/game/npc";
+export { Market } from "./entities/game/market";
+export { City } from "./entities/game/city";
+export { Building } from "./entities/game/building";
+export { Position } from "./entities/game/geometry/position";
+export { Offer } from "./entities/game/market/offer";
+export { Sale } from "./entities/game/market/sale";
+
 // Open connection postgress function
 let openConnection = (async ( param: ConectionParameter = undefined)=>{
 
