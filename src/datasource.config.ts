f684690a52e39c56e256102d08b679b07f48f522
Gilberto <38070920+gilberto-009199@users.noreply.github.com>
2023-06-02 23:22:37 -0300

    Update datasource.config.ts

--- src/datasource.config.ts
+++ src/datasource.config.ts
@@ -1,11 +1,13 @@
 import { DataSource } from 'typeorm';
 
 
-const HOST = process.env.HOST || "localhost";
-const PORT = Number(process.env.PORT || "5432");
-const USERNAME = process.env.USERNAME || "root";
-const PASSWORD = process.env.PASSWORD || "123456";
-const DATABASE = process.env.DATABASE || "warlords";
+
+const HOST = process.env.POSTGRES_HOST || "localhost";
+const PORT = Number(process.env.POSTGRES_PORT || "5432");
+const USERNAME = process.env.POSTGRES_USER || "root";
+const PASSWORD = process.env.POSTGRES_PASSWORD || "123456";
+const DATABASE = process.env.POSTGRES_DB || "warlords";
+
 /*
 const HOST = "localhost";
 const PORT = Number("5432");
